3a8faa45c0ad3cfb4d4a54e9dc85c6d73e5a1a70
LHufton <lukehufton@gmail.com>
2024-05-09 02:14:09 -0500

    app.jsx built

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Profile from './components/Profile/Profile'
+import Playlist from './components/Playlist/Playlist'
+import ArtistSearch from './components/ArtistSearch/ArtistSearch'
 
-const App = () => {
-  return <></>
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/playlist" component={Playlist} />
+        <Route path="/artist-search" component={ArtistSearch} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
