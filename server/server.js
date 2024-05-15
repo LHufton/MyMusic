a9ec2073aa982d9315582ab5987406b6f143b660
LHufton <lukehufton@gmail.com>
2024-05-14 23:50:20 -0500

    both server and client running

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const spotifyRoutes = require('./routes/Spotify')
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.json())
 app.use('/spotify', spotifyRoutes)
