147c2ff88aa1e59b4082a43f7d3f1bca52390d6a
LHufton <lukehufton@gmail.com>
2024-05-14 22:55:16 -0500

    index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import App from './components/App'
+import Profile from './components/Profile'
+import Login from './components/Login'
+import Playlist from './components/Playlist'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/playlist" element={<Playlist />} />
+      <Route path="/app" element={<App />} />
+    </Routes>
+  </Router>
 )
