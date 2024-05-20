b40310f912ed571a6d24e932a17e661e7e67dbc9
LHufton <lukehufton@gmail.com>
2024-05-19 19:40:08 -0500

    reinstalled dependencies

--- client/src/components/Playlist/Playlist.jsx
+++ client/src/components/Playlist/Playlist.jsx
@@ -10,7 +10,7 @@ const Playlist = ({ token }) => {
 
   const createPlaylist = async () => {
     try {
-      const userResponse = await axios.get('http://localhost:5173/spotify/me', {
+      const userResponse = await axios.get('http://localhost:5000/spotify/me', {
         headers: {
           Authorization: `Bearer ${token}`
         }
@@ -18,7 +18,7 @@ const Playlist = ({ token }) => {
 
       const userId = userResponse.data.id
       await axios.post(
-        'http://localhost:5173/spotify/playlist',
+        'http://localhost:5000/spotify/playlist',
         {
           userId,
           playlistName,
