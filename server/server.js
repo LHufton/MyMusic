38ec7ab4f0899ed92b1bad822b526ba32cd81f10
LHufton <lukehufton@gmail.com>
2024-06-26 22:39:04 -0500

    new localtunnel URL and password

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-import fs from 'fs'
-import https from 'https'
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
@@ -7,15 +5,10 @@ import { fileURLToPath } from 'url'
 import session from 'express-session'
 import spotifyRoutes from './routes/Spotify.js'
 
-// Load environment variables
-dotenv.config() // Ensure this is at the very top
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-console.log('CLIENT_ID:', process.env.CLIENT_ID)
-console.log('REDIRECT_URI:', process.env.REDIRECT_URI)
-console.log('SESSION_SECRET:', process.env.SESSION_SECRET)
 
 const app = express()
 const PORT = 5001
@@ -31,17 +24,6 @@ app.use(
 
 app.use('/api', spotifyRoutes)
 
-// Force HTTPS redirection
-app.use((req, res, next) => {
-  if (
-    req.headers['x-forwarded-proto'] !== 'https' &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''))
-  }
-  next()
-})
-
 const frontendPath = path.join(__dirname, '../client/dist')
 app.use(express.static(frontendPath))
 
@@ -49,18 +31,6 @@ app.get('*', (req, res) => {
   res.sendFile('index.html', { root: frontendPath })
 })
 
-try {
-  const keyPath = path.resolve(__dirname, 'SS1/localhost-key.pem')
-  const certPath = path.resolve(__dirname, 'SS1/localhost.pem')
-
-  const options = {
-    key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
-  }
-
-  https.createServer(options, app).listen(PORT, () => {
-    console.log(`HTTPS server running on port ${PORT}`)
-  })
-} catch (error) {
-  console.error('Error starting HTTPS server:', error.message)
-}
+app.listen(PORT, () => {
+  console.log(`HTTP server running on port ${PORT}`)
+})
